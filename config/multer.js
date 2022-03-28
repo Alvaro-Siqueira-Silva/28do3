147d42cc9de92a2170cca745c5cf8679704b4468
Alvaro-Siqueira-Silva <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2022-03-28 12:59:44 -0300

    Add files via upload

---
+++ config/multer.js
@@ -0,0 +1,14 @@
+const multer = require ('multer')
+
+const armazenamento = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./assets/images/')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now() +file.originalname)
+    }
+})
+
+var upload = multer({storage:armazenamento})
+
+module.exports = upload
